11bee7e8bcebaab97030883613e86931b7a5f8e6
Venkata Nagendra Rao Saragadam <svnagendra@gmail.com>
2018-06-18 16:05:03 +0530

    Added Services Layer

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ var express = require('express');
 
 var app = express();
 
+//Refer all required folders & references
+api = require('./services/api'),
+
 app.get('/', function(req, res) {
   res.send({
     "Output": "Hello World!"
   });
 });
 
-app.get('/name', function(req, res) {
-  res.send({
-    "Output": "Hello World! My Name is Test."
-  });
-});
+app.get('/name', api.name);
 
 app.post('/', function(req, res) {
   res.send({
